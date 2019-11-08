d6e6674efeaa44fb539e2ee6193fc738d0e7cb6e
Alekslol <lexa-shaforostov.shaforostov@yandex.ru>
2019-11-08 12:32:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KUBS
+{
+    class Program
+    {
+ 
+
+        static void Main(string[] args)
+        {
+            int[,] mas = NewMethod();
+            int rows = mas.GetUpperBound(0) + 1;
+            int columns = mas.Length / rows;
+            for (int i = 0; i < rows; i++) ;
+
+        }
+
+        private static int[,] NewMethod()
+        {
+            return { { 1, V, 3 }, { v, 5, 6 }, { 7, 8, 9 }, { 00, 00, 00 }, { 00, 77, 77 } };
+        }
+    }
+}
---
+++ kub/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace kub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, i = 1;
+            size = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 0; x <= size+1; x++)
+            {
+                if ((x == 0) || (x == size+1))
+                {
+                    while (i <= size)
+                    {
+                        if (i == size) { Console.WriteLine("_ "); }
+
+                        else { Console.Write("_ "); }
+                        i++;
+                    }
+                    i = 1;
+
+                }
+                else
+                {
+                    Console.Write("! ");
+                    while (i <= size-1)
+                    {
+                        Console.Write("  ");
+                        i++;
+                          
+                    }
+                    i = 1;
+                    Console.WriteLine("!");
+                    
+                }
+            }
+
+        }
+    }
+}
+
+
+
+        
+
+
+
+
+
+ 
